da1b1752c007faf0e30af9f7f9b00cb75cc6a1ab
Felipe Lima <felip.3lima@hotmail.com>
2020-08-28 20:13:14 -0300

    Initial commmit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const App: React.FC = () => {
+  return <h1>Felipe</h1>;
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.tsx'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development',
+  resolve: {
+    extensions: ['.jsx', '.js', '.tsx', '.ts'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html/,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.tsx?$/,
+        use: ['ts-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+    }),
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    port: 8888,
+  },
+};
