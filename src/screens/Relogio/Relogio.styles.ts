34e3380320279a89e8549db5d3cbc56aa007313c
Gustavo Cunha <gustavo.oliveira-98@hotmail.com>
2020-10-15 11:41:14 -0300

    First commit with structuring of screens and components

---
+++ src/screens/Relogio/Relogio.styles.ts
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    background: #d0f0fa;
+    width: 100%;
+    height: 100%;
+
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+`;
+
+export const Display = styled.div`
+    justify-content: center;
+    align-items:center;
+    display: flex;
+`;
+
+export const ComponentDisplay = styled.span`
+    background: #d1d1d1;
+    font-size: 72px;
+    line-height: 50px;
+    padding: 10px;
+    margin: 5px;
+    border-radius: 5px;
+`;
+
+export const Button = styled.button`
+    background: #d1d1d1;
+    border-radius: 15px;
+    padding: 10px;
+`;
